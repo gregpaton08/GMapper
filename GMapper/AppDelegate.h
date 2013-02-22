ed92915e836b4de7cf30992b32544628430bcba5
gregpaton08 <gregpaton08@gmail.com>
2013-02-22 00:29:03 -0500

    Initial Commit

---
+++ GMapper/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GMapper
+//
+//  Created by Greg Paton on 2/22/13.
+//  Copyright (c) 2013 Greg Paton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ GMapper/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GMapper
+//
+//  Created by Greg Paton on 2/22/13.
+//  Copyright (c) 2013 Greg Paton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
